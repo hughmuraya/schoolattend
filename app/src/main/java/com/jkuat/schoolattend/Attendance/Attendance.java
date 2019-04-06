7b22bd6531aba8fd7670b84d84cc0d3f9cf09633
hughn <hughmuraya@gmail.com>
2019-04-06 15:15:56 +0300

    QR Code Implementation
    Signed-off-by: hughn <hughmuraya@gmail.com>

--- app/src/main/java/com/jkuat/schoolattend/Attendance/Attendance.java
+++ app/src/main/java/com/jkuat/schoolattend/Attendance/Attendance.java
@@ -3,13 +3,19 @@ package com.jkuat.schoolattend.Attendance;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.zxing.Result;
 import com.jkuat.schoolattend.R;
 
-public class Attendance extends AppCompatActivity {
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+public class Attendance extends AppCompatActivity  {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_attendance);
     }
+
 }
